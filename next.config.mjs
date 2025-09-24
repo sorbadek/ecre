f65626c4bec58ce3a1ee3e731873f8e28fabc4c7
MichAdek23 <adelekem600@gmail.com>
2025-09-24 12:16:10 +0100

    major

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  swcMinify: true,
+  productionBrowserSourceMaps: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
